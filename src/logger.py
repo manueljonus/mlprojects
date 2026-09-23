6ea19a5fd444a4c39e631e30ae077feb48e284f1
manueljonus97 <manuel.da19@iiitmk.ac.in>
2026-09-23 23:14:26 +0530

    Data Transformation Done

--- src/logger.py
+++ src/logger.py
@@ -9,6 +9,8 @@ os.makedirs(logs_path,exist_ok=True)
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO,format='[%(asctime)s] %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-# if __name__=="__main__":
-#     logging.info("Logging has started.")
+if __name__=="__main__":
+    logging.info("Logging has started.")
+
+
 
